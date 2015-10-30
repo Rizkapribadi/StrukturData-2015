e6e1c093d522f0143f56ba96a544b2258a495693
Rizkapribadi <rizka.pribadi@gmail.com>
2015-10-30 16:33:33 +0700

    code saya

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class Vector3D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+class Vector3D
+{
+    // instance variables - replace the example below with your own
+   double x,y,z;
+        public double x(){
+            
+         return x  ;  
+            
+        }
+        public double y(){ 
+            return y ;
+        }
+        public double z(){
+            
+           return z;
+        }
+    /**
+     * Constructor for objects of class Vector3D
+     */
+   
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public Vector3D (double x, double y,double z)
+    {
+        // put your code here
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
