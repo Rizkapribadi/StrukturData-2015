ce56c7d534892bcd4126ec83902508323488efe0
Rizkapribadi <rizka.pribadi@gmail.com>
2015-12-11 23:05:12 +0700

    Tugas Tebak Angka

---
+++ TebakAngka/UtamaClient.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+public class UtamaClient {
+    public static void main(String[] args)
+    throws UnknownHostException, IOException {
+        Client client = new Client();
+        client.chat();
+    }
+}
